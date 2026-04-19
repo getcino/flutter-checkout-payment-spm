1617ba7e8ca0fd8b11e5dcd6db241d9fe9f768e4
Adrian Rosian <hq@whitecitycode.com>
2026-04-19 17:38:37 +0300

    chore(frames): update frames version

--- android/src/main/java/com/shadyboshra2012/flutter_checkout_payment/FlutterCheckoutPaymentPlugin.java
+++ android/src/main/java/com/shadyboshra2012/flutter_checkout_payment/FlutterCheckoutPaymentPlugin.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 
-import android.view.View;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 
